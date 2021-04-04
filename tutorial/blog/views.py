32762f1879fe348dcc268ad09d92a19d3f08a32b
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-04-04 18:50:08 +0200

    Tutorial part-2

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,3 +1,8 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {"name": "Primo blog con django"}
+    return render(request, 'index.html', context)
+
